58c93392f0fd6f488becbe94299902e863ad55e6
CeilingG <42397975+Shalomguan@users.noreply.github.com>
2025-11-25 18:08:24 +0800

    123

--- part2/example/src/App.jsx
+++ part2/example/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import Note from './components/Note'
+import noteService from './services/notes'
+
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState(
@@ -12,20 +14,23 @@ const App = () => {
     console.log('effect')
     axios
       .get('http://localhost:3001/notes')
-      .then(response => {
-        console.log('promise fulfilled')
-        setNotes(response.data)
+      noteService
+      .getAll()
+      .then(initialNotes => {
+        setNotes(initialNotes)
       })
    }, [])
   
   console.log('render', notes.length, 'notes')
   const toggleImportanceOf = (id) => {
-    const url = `http://localhost:3001/notes/${id}`
     const note = notes.find(n => n.id === id)
     const changedNote = { ...note, important: !note.important }
-    axios.put(url, changedNote).then(response => {
-      setNotes(notes.map(note => note.id === id ? response.data : note))
-    })
+
+    noteService
+      .update(id, changedNote)
+      .then(response => {
+        setNotes(notes.map(note => note.id === id ? response.data : note))
+      })
   }
   const addNote = (event) => {
     event.preventDefault()
@@ -35,15 +40,12 @@ const App = () => {
       important: Math.random() < 0.5,
       
     }
-    axios
-    .post('http://localhost:3001/notes', noteObject)
-    .then(response => {
-      console.log(response)
-      setNotes(notes.concat(response.data))
-      setNewNote('')
-    })
-    setNotes(notes.concat(noteObject))
-    setNewNote(' ')
+    noteService
+      create(noteObject)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
+        setNewNote('')
+      })
   }
   const notestoshow = showAll ?
     notes : notes.filter(note => note.important === true)
